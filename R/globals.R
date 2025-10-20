a47e05256c496c24340058a7b9e9b18a1595c1f2
João Paulo Barbosa <joao.31582129@gmail.com>
2025-10-19 22:30:13 -0500

    add get_inflation_rate() function - v0.3.0

--- R/globals.R
+++ R/globals.R
@@ -6,6 +6,10 @@ utils::globalVariables(c("data", "valor", "Valor", "trimestre", "ultimo_mes",
 
                          "month", "year", "rate",
 
+                         #get_inflation_rate
+
+                         "monthly_inflation", "ytd_inflation", "twelve_month_inflation",
+
                          #plot_unemployment
 
                          "ylab"))
